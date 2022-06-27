cc4082c5f406d24ef0a34abc6b301ac82baa667b
Muhammad Farid Ullah <faridkhan125janan@gmail.com>
2022-06-27 10:46:40 +0500

    Initial Commit

---
+++ Casino/CasinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CasinoApp.swift
+//  Casino
+//
+//  Created by Muhammad Farid Ullah on 27/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct CasinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
